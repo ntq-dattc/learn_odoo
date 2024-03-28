c298e3d9a0d0b80d5d6b905392546c7052141d38
DungNM <dung.ngo1@ntq-solution.com.vn>
2024-03-28 13:45:41 +0700

    buoi 2

--- addons_tests/test_module/models/hop_dong.py
+++ addons_tests/test_module/models/hop_dong.py
@@ -1,19 +1,37 @@
+from mock.mock import self
+
 from odoo import models, fields
+import calendar
+from datetime import *
+
+
+def get_signed_date():
+    signed_date =  datetime.now() + timedelta(weeks=1)
+    if signed_date.weekday() == 5:
+        signed_date += timedelta(days=2)
+    elif signed_date.weekday() == 6:
+        signed_date += timedelta(days=1)
+    return signed_date
 
 
 class hop_dong(models.Model):
     _name = 'hop.dong'
     _description = 'hop dong'
 
-    name = fields.Char(string='ten')
-    start_date = fields.Datetime(string='ngay bat dau')
-    end_date = fields.Datetime(string='ngay ket thuc')
-    contract_type = fields.Char(string='loai hop dong')
-    signed_date = fields.Datetime(string='ngay ky')
-    salary_rack = fields.Float(string='luong ngach bac')
-    efficiency_wage = fields.Float(string='luong hieu qua')
+    def get_running_contracts(self):
+        return
+
+    name = fields.Char(string='Name')
+    start_date = fields.Datetime(string='Start Date: ', default=fields.Datetime.now, format='%Y-%m-%d')
+    end_date = fields.Datetime(string='End Date: ')
+    contract_type = fields.Char(string='Contract Type: ')
+    signed_date = fields.Datetime(string='Signed Date: ', default=get_signed_date())
+    salary_rack = fields.Float(string='Standard Salary: ')
+    efficiency_wage = fields.Float(string='Efficiency Wage: ')
     status = fields.Selection([
-        ('draft', 'ban nhap'),
-        ('ongoing', 'dang dien ra'),
-        ('expired', 'het han')],
-        string='trang thai', default='draft')
+        ('new', 'New'),
+        ('running', 'Running'),
+        ('expired', 'Expired'),
+        ('pause', 'Pause')],
+        string='Status', default='new')
+    running_contracts = fields.
